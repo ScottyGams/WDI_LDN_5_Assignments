f375c0515f9c84269ad43a00427681f41a93aa02
ScottyGams <ScottyGammon@gmail.com>
2014-05-27 10:00:54 +0100

    up and running with notes backbone app

---
+++ ScottyGammon/9Week/2Dw9/backbone-notepad/app/controllers/static_controller.rb
@@ -0,0 +1,4 @@
+class StaticController < ApplicationController
+  def notes
+  end
+end
---
+++ ScottyGammon/9Week/2Dw9/backbone-notepad/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneNotepad::Application.config.secret_token = '425ed840c2a444a8d903b5b8379959fed196cabb4501464a2b0d242bbdc76ceffc0723a459a8ffc74ade59f9d5af2fe24e41bd84f3d38f3baea38bb5b1e10afa'
